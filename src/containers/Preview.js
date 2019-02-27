932fa9456a6ab0336270e41a527872477a56ace1
Carmen V Ramos <carmenvramos@yahoo.com>
2019-02-27 15:11:27 -0800

    Convert Preview to PureComponent

--- src/containers/Preview.js
+++ src/containers/Preview.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import marked from 'marked';
 import styles from 'github-markdown-css/github-markdown.css';
-import { getMarkdown } from '../selectors/document';
-import store from '../store';
+import PropTypes from 'prop-types';
 
-function Preview() {
-  const __html = marked(getMarkdown(store.getState()));
-  return <div className={styles['markdown-body']} dangerouslySetInnerHTML={{ __html }}></div>;
+export default class Preview extends PureComponent {
+  static propTypes = {
+    markdown: PropTypes.string.isRequired
+  }; 
+
+  render() {
+    const __html = marked(this.props.markdown);
+    return <div className={styles['markdown-body']} dangerouslySetInnerHTML={{ __html }}></div>;
+  }
 }
-
-export default Preview;
