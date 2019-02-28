41073a743d6409ca556785f208d4b44f5674f1ea
Shaba9 <shaba92@gmail.com>
2019-02-28 11:16:41 -0800

    actions UPDATE_FILES test added

--- src/actions/document.test.js
+++ src/actions/document.test.js
@@ -1,11 +1,26 @@
-import { updateMarkdown } from './document';
+import { 
+  updateMarkdown,
+  updateFiles
+} from './document';
 
 describe('action test', () => {
-  it('returns action', () => {
+  it('returns action for UPDATE_MARKDOWN', () => {
     const updated = updateMarkdown('# How are you');
     expect(updated).toEqual({
       type: 'UPDATE_MARKDOWN',
       payload: '# How are you'
+    });
+  });
+
+  it('returns action for UPDATE_FILES', () => {
+    const file = {
+      id: 5,
+      title: 'z'
+    };
+    const updated = updateFiles(file);
+    expect(updated).toEqual({
+      type: 'UPDATE_FILES',
+      payload: file
     });
   });
 }); 
