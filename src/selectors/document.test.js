f84fccef13a77a16396749e3719b5c4ac07b2538
Shaba9 <shaba92@gmail.com>
2019-02-28 11:23:57 -0800

    selector getFiles test added

--- src/selectors/document.test.js
+++ src/selectors/document.test.js
@@ -1,13 +1,47 @@
-import { getMarkdown } from './document';
+import { 
+  getMarkdown,
+  getFiles
+} from './document';
 
 describe('selector test', () => {
+  const state = {
+    markdown: '# How are you',
+    selectedFile: 0,
+    files: [
+      {
+        id: 0,
+        title: 'a'
+      },
+      {
+        id: 1,
+        title: 'b'
+      },
+      {
+        id: 2,
+        title: 'c'
+      }
+    ]
+  };
   it('returns markdown', () => {
-    const state = {
-      markdown: '# How are you'
-    };
-
     const markdown = getMarkdown(state);
-
     expect(markdown).toEqual('# How are you');
+  });
+
+  it('returns list of files', () => {
+    const files = getFiles(state);
+    expect(files).toEqual([
+      {
+        id: 0,
+        title: 'a'
+      },
+      {
+        id: 1,
+        title: 'b'
+      },
+      {
+        id: 2,
+        title: 'c'
+      }
+    ]);
   });
 });
