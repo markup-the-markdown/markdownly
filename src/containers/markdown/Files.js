4daea23427673b95f703ad2395cfc03f9a32053b
Shaba9 <shaba92@gmail.com>
2019-03-01 10:10:57 -0800

    Files container refactored to use connect

--- src/containers/markdown/Files.js
+++ src/containers/markdown/Files.js
@@ -1,37 +1,11 @@
-import React from 'react';
 import { connect } from 'react-redux';
 import { getTitles } from '../../selectors/document';
 import FilesDisplay from '../../components/FilesDisplay';
 
-function Files({ titles }) {
-  return (
-    <FilesDisplay
-      titles={titles}
-    />
-  );
-}
-  
 const mapStateToProps = (state) => ({
   titles: getTitles(state)
 });
 
-const mapDispatchToProps = dispatch => ({
-  
-})
-  componentDidMount() {
-    this.updateFiles();
-    this.unsubscribe = store.subscribe(this.updateFiles);
-  }
+const FilesContainer = connect(mapStateToProps)(FilesDisplay);
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  updateFiles = () =>  this.setState({ titles: getTitles(store.getState()) });
-
-  render() {
-    const { titles } = this.state;
-
-    return 
-  }
-}
+export default FilesContainer;
