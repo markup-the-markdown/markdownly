f99d53013d2a41a0b52c808d11c3ef271bd0cb8b
Shaba9 <shaba92@gmail.com>
2019-03-01 10:26:25 -0800

    title state added to reducer, selector, and action

--- src/reducers/document.js
+++ src/reducers/document.js
@@ -4,6 +4,7 @@ import {
 } from '../actions/document';
 
 const initialState = {
+  title: '',
   markdown: '# Hi there',
   selectedFile: 0,
   files: [
@@ -36,7 +37,12 @@ export default function reducer(state = initialState, action) {
           ...state.files,
           action.payload
         ]
-      };  
+      };
+    case UPDATE_TITLE:
+      return {
+        ...state,
+        title: action.payload
+      };
     default:
       return state;  
   }
--- src/selectors/document.js
+++ src/selectors/document.js
@@ -3,3 +3,5 @@ export const getMarkdown = state => state.markdown;
 export const getFiles = state => state.files;
 
 export const getTitles = state => state.files.map(file => file.title);
+
+export const getTitle = state => state.title;
